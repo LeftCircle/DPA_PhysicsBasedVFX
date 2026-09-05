8dc475e1dda1d1aad54518ccd0ce9ed67e4f8ae0
left <left@localhost.localdomain>
2026-09-04 20:38:14 -0400

    Starting to set up first render

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 
+#include "field_interface.h"
+#include "ray_marcher.h"
+#include "image_data.h"
 
+
+using namespace lux;
 
 int main(int argc, char** argv){
+
+	ImageData test_image(64, 64, 4);
+	RayMarcher rm;
+	rm.set_ds(0.01);
+	rm.set_snear(0);
+	rm.set_sfar(3);
+	rm.set_exticntion_coefficient(1.0);
+	rm.set_Tmin(0.01);
+	
+	
+
+	VolumeSPtr<float> a = isf_sphere(Vector(), 1.0);
 
 	return 0;
 }
