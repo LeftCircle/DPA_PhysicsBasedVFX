d16da55a0b098e839a5f1d3000b5e73ca0e77f27
LeftCircle <leftcircle@LeftStation.>
2026-09-09 11:53:22 -0400

    starting humanoid

---
+++ src/humanoid.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <memory>
+
+#include "field_interface.h"
+#include "ray_marcher.h"
+#include "image_data.h"
+#include "command_line_parser.h"
+
+
+
+
+void ray_march_humanoid(){
+    
+}
